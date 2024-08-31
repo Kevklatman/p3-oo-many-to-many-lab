a35cce460d0c2e7f24c7496387a764694560727e
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-31 13:44:28 -0600

    lab complete

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,59 +1,63 @@
+from datetime import datetime
+
+class Book:
+    all = []
+
+    def __init__(self, title):
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string.")
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 class Author:
     all = []
 
     def __init__(self, name):
         if not isinstance(name, str):
-            raise TypeError("Name must be type of string.")
-        self.name=name
+            raise TypeError("Name must be a string.")
+        self.name = name
         Author.all.append(self)
-    
-    def contracts(self): #This method should return a list of related contracts.
-        pass
 
-    def books(self): #This method should return a list of related books using the Contract class as an intermediary.
-        pass
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
 
-    def sign_contract(book, date, royalties): #This method should create and return a new Contract object between the author and the specified book with the specified date and royalties
-        pass
+    def books(self):
+        return [contract.book for contract in self.contracts()]
 
-    def total_royalties(): #This method should return the total amount of royalties that the author has earned from all of their contracts.
-        pass
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
 
-
-
-
-
-class Book:
-    all = []
-    def __init__(self, title):
-        if not isinstance(title, str):
-            raise TypeError("Title must be type of string.")
-        self.title=title
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 class Contract:
-    
     all = []
 
     def __init__(self, author, book, date, royalties):
-        if not isinstance(date, date):
-            raise TypeError("Value is not a valid date.")
-        self.date=date
+        if not isinstance(author, Author):
+            raise TypeError("Author must be an instance of the Author class.")
+        self.author = author
+
+        if not isinstance(book, Book):
+            raise TypeError("Book must be an instance of the Book class.")
+        self.book = book
+
+        if not isinstance(date, str):
+            raise TypeError("Date must be a string.")
+        self.date = date
+
         if not isinstance(royalties, int):
             raise TypeError("Royalties must be an integer.")
-        self.royalties=royalties
-        if not isinstance(author, str):
-            raise TypeError("Author must be type of string.")
-        self.author=author
-        if not isinstance(book, str):
-            raise TypeError("Book must be type of string.")
-        self.book=book
+        self.royalties = royalties
+
         Contract.all.append(self)
 
-    def contracts_by_date(cls, date): #A class method contracts_by_date(cls, date): This method should return all contracts that have the same date as the date passed into the method.
-
-
-        
-
-
-
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
