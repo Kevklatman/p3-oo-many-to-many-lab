4d6c13d0de5baff85906936e0b92e40ced999ecf
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-31 11:29:53 -0600

    more exceptions

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -3,7 +3,6 @@ class Author:
         if not isinstance(name, str):
             raise TypeError("Name must be type of string.")
         self.name=name
-all =[]
 
 
 
@@ -12,10 +11,14 @@ class Book:
         if not isinstance(title, str):
             raise TypeError("Title must be type of string.")
         self.title=title
-all = []
 
 
 class Contract:
     def __init__(self, author, book, date, royalties):
-        pass
-all = {}
+        if not isinstance(date, date):
+            raise TypeError("Value is not a valid date.")
+        self.date=date
+        if not isinstance(royalties, int):
+            raise TypeError("Royalties must be an integer.")
+        self.royalties=royalties
+        
