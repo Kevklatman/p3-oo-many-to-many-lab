f310af8c9e1a7bda20039d8e08d9cd3ca0d84e05
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-31 11:15:11 -0600

    exceptions

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,13 +1,21 @@
 class Author:
     def __init__(self, name):
-        pass
+        if not isinstance(name, str):
+            raise TypeError("Name must be type of string.")
+        self.name=name
+all =[]
+
 
 
 class Book:
     def __init__(self, title):
-        pass
+        if not isinstance(title, str):
+            raise TypeError("Title must be type of string.")
+        self.title=title
+all = []
 
 
 class Contract:
     def __init__(self, author, book, date, royalties):
         pass
+all = {}
